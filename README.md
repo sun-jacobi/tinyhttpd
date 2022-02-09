28ed97db8ce9fdab967fa0c39093154fa2a5b1fc
sun-jacobi <sun1011jacobi@gmail.com>
2022-02-09 17:28:41 +0900

    0209

--- README.md
+++ README.md
@@ -13,3 +13,27 @@ Here I will write some preliminary knowledge.
 
 Host is identified by IP address, which is a 32-bit quantity. Sending process need to identify the process on the receiving host, since a host could run many application. Port are used to do this.
 
+
+### Socket programming 
+Reference : https://www.geeksforgeeks.org/socket-programming-cc/
+
+
+>Socket programming is a way
+to connect 2 nodes on a network
++ Socket creation: 
+```
+int sockfd = socket(domain,type,protocal)
+```
+domain: commnunication domain
+*AP_INET and AF_I NET 6 for IPV6*
+type: communication type
+* SOCK_STREAM : TCP
+* SOCK_DGRAM  : UDP
+
+Protocal : value for the IP is 0
+
+
+
+
+
+
--- http.c
+++ http.c
@@ -7,10 +7,41 @@
 #include <stdlib.h> 
 
 int startup(u_short *);
-int error_die(const char*);
+void error_die(const char*);
 void accept_request(int);
 
+void error_die(const char *sc){
+    perror(sc);
+    exit(1);
+}
 
+int startup(u_short *port)
+{
+    int httpd = 0;
+    struct sockaddr_in name;
+    httpd = socket(PF_INET,SOCK_STREAM,0);
+    if (httpd == -1){
+        error_die("socket");
+    }
+    memset(&name, 0, sizeof(name));
+    name.sin_family = AF_INET;
+    name.sin_port = htons(*port);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(httpd,(struct sockaddr*)&name,sizeof(name)) < 0 ){
+        error_die("bind");
+    }
+    if (*port == 0){
+        int namelen = sizeof(name);
+        if (getsockname(httpd,(struct sockaddr*)&name,&namelen) == -1){
+            error_die("getsockname");
+        };
+        *port = ntohs(name.sin_port);   
+    }
+    if (listen(httpd,5) < 0){
+        error_die("bind");      
+    }
+    return(httpd);
+}
 int main(void){
     int server_sock = -1;
     u_short port = 0; /* unsigned short in <sys/types.h> */ 
@@ -24,12 +55,12 @@ int main(void){
     while(1){
         client_sock = accept(server_sock,
         (struct sockaddr*)&client_name,
-        &client_name_len);
+        &client_name_len); 
         if (client_sock == -1){
             error_die("accept");
-        }       
+        } /* accept client socket fail -> exit  */    
         if (pthread_create(&newthread,NULL,accept_request,client_sock) != 0){
-            perror("pthread_create");
+            perror("pthread_create"); /*p_thread create fail  */
         }
     }
     close(server_sock);
